40d838d9a55be62c7cfd82d0ad1e8e76bab3eca9
xxgrkua <xxgrkua@users.noreply.github.com>
2024-01-10 16:06:58 +0900

    Add api signature for server-side

--- server/server.js
+++ server/server.js
@@ -1,21 +1,68 @@
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const portno = 5000; // Port number to use
 
-const app = express();
-
-app.use(express.static("build"));
-
-app.get("/", function (request, response) {
-  response.send("Simple web server of files from " + __dirname);
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1/turtle-draw", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const server = app.listen(portno, function () {
-  const port = server.address().port;
-  console.log(
-    "Listening at http://localhost:" +
-      port +
-      " exporting the directory " +
-      __dirname,
+function isAuthenticated(request, response, next) {
+  if (request.session.user_id && request.session.username) {
+    next();
+  } else {
+    response.status(401).json({ res: "error", msg: "Unauthorized user" });
+  }
+}
+
+async function main() {
+  const app = express();
+
+  app.use(
+    session({
+      secret: "secret key for photo app",
+      resave: false,
+      saveUninitialized: false,
+    }),
   );
+  app.use(bodyParser.json());
+  app.use(express.static("build"));
+
+  app.get("/", function (request, response) {
+    response.send("Simple web server of files from " + __dirname);
+  });
+
+  app.get("/user/info", isAuthenticated, async function (request, response) {});
+
+  app.post("/user/login", async function (request, response) {});
+
+  app.post("/user/logout", async function (request, response) {});
+
+  app.post("/user/register", async function (request, response) {});
+
+  app.get("/gallery/list", async function (request, response) {});
+
+  app.post(
+    "/user/draw",
+    isAuthenticated,
+    async function (request, response) {},
+  );
+
+  return app;
+}
+
+main().then((app) => {
+  const server = app.listen(portno, function () {
+    const port = server.address().port;
+    console.log(
+      "Listening at http://localhost:" +
+        port +
+        " exporting the directory " +
+        __dirname,
+    );
+  });
 });
