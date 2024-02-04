0456202f5587cb7f10d01b574f3e33ad05e9bc1d
xxgrkua <xxgrkua@users.noreply.github.com>
2024-02-05 04:29:50 +0900

    Add server config

---
+++ config/development.ts
@@ -0,0 +1,5 @@
+export default {
+  DATABASE_URL: "mongodb://127.0.0.1/turtle-draw",
+  DATABASE_NAME: "turtle-draw",
+  PORT: 5000,
+};
---
+++ config/production.ts
@@ -0,0 +1,5 @@
+export default {
+  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  PORT: process.env.PORT || 5000,
+};
