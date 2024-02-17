b3ff785d41cd3f665640b3e92fb84d1a7b786d78
xxgrkua <xxgrkua@users.noreply.github.com>
2024-02-18 02:33:53 +0900

    Fix import bug

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -4,7 +4,7 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { counterSlice } from "../features/counter/counterSlice";
 import { quotesApiSlice } from "../features/quotes/quotesApiSlice";
 import { userSlice } from "../features/user";
-import { workbenchSlice } from "../features/workspace";
+import { workbenchSlice } from "../features/workbench";
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
