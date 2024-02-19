76daaa8071f1cd16cdc11eb932827ecc8b77e95f
xxgrkua <xxgrkua@users.noreply.github.com>
2024-02-20 03:40:31 +0900

    Improve chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), wasm(), topLevelAwait(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("ace-builds")) {
+            return "ace-builds";
+          }
+          if (id.includes("@mui")) {
+            return "mui";
+          }
+        },
+      },
+    },
+  },
 });
